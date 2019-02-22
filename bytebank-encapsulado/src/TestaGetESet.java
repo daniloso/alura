423a10c1d1f1ef2703f72b87c3474d718b5a5821
daniloso <daniloso20@gmail.com>
2019-02-22 17:14:33 -0300

    Finalizado curso de introdução a orientação a objetos

--- bytebank-encapsulado/src/TestaGetESet.java
+++ bytebank-encapsulado/src/TestaGetESet.java
@@ -2,8 +2,18 @@
 public class TestaGetESet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Conta conta = new Conta(123, 456);
+		Conta contaDoPaulo = new Conta(123, 321);
+		
+		conta.setTitular(new Cliente()); 
+		//ou
+		Cliente paulo = new Cliente();
 
+		conta.getTitular().setNome("Danilo");
+		conta.deposita(100);
+		
+		System.out.println(conta.getTitular().getNome()+ " tem "+conta.getSaldo()+" de saldo!");
+		System.out.println("Total de contas é: "+Conta.getTotal());
 	}
 
 }
